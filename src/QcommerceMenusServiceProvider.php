5382903f0bcf03c64c3508f5dabf8eee7c679dbb
Robin van Maasakker <robin@qubiqx.com>
2022-02-26 10:28:33 +0100

    Update

--- src/QcommerceMenusServiceProvider.php
+++ src/QcommerceMenusServiceProvider.php
@@ -5,6 +5,8 @@ namespace Qubiqx\QcommerceMenus;
 use Filament\PluginServiceProvider;
 use Qubiqx\QcommerceMenus\Filament\Resources\MenuItemResource;
 use Qubiqx\QcommerceMenus\Filament\Resources\MenuResource;
+use Qubiqx\QcommerceMenus\Models\Menu;
+use Qubiqx\QcommerceMenus\Models\MenuItem;
 use Spatie\LaravelPackageTools\Package;
 
 class QcommerceMenusServiceProvider extends PluginServiceProvider
@@ -14,6 +16,9 @@ class QcommerceMenusServiceProvider extends PluginServiceProvider
     public function configurePackage(Package $package): void
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        cms()->model('Menu', Menu::class);
+        cms()->model('MenuItem', MenuItem::class);
 
         $package
             ->name('qcommerce-menus');
