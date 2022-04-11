2dace9c60693429f7cfb57644c163d23d0f3c13d
Robinvm <Robinvm@users.noreply.github.com>
2022-04-11 12:10:09 +0000

    Fix styling

--- src/Filament/Resources/MenuItemResource.php
+++ src/Filament/Resources/MenuItemResource.php
@@ -3,21 +3,21 @@
 namespace Qubiqx\QcommerceMenus\Filament\Resources;
 
 use Closure;
-use Illuminate\Support\Str;
-use Filament\Resources\Form;
-use Filament\Resources\Table;
-use Filament\Resources\Resource;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\Section;
-use Filament\Tables\Columns\TextColumn;
-use Qubiqx\QcommerceCore\Classes\Sites;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\MultiSelect;
-use Qubiqx\QcommerceMenus\Models\MenuItem;
-use Filament\Resources\Concerns\Translatable;
 use Filament\Forms\Components\BelongsToSelect;
-use Qubiqx\QcommerceMenus\Filament\Resources\MenuItemResource\Pages\EditMenuItem;
+use Filament\Forms\Components\MultiSelect;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Concerns\Translatable;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Support\Str;
+use Qubiqx\QcommerceCore\Classes\Sites;
 use Qubiqx\QcommerceMenus\Filament\Resources\MenuItemResource\Pages\CreateMenuItem;
+use Qubiqx\QcommerceMenus\Filament\Resources\MenuItemResource\Pages\EditMenuItem;
+use Qubiqx\QcommerceMenus\Models\MenuItem;
 
 class MenuItemResource extends Resource
 {
