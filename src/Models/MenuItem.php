c51da259c33a67d8c0defc24fd85fabfb84614e0
Robinvm <Robinvm@users.noreply.github.com>
2022-05-13 07:36:41 +0000

    Fix styling

--- src/Models/MenuItem.php
+++ src/Models/MenuItem.php
@@ -112,7 +112,7 @@ class MenuItem extends Model
         $menuItem = $this;
         while ($menuItem->parent_menu_item_id) {
             $menuItem = self::find($menuItem->parent_menu_item_id);
-            if (!$menuItem) {
+            if (! $menuItem) {
                 return;
             }
         }
@@ -132,7 +132,7 @@ class MenuItem extends Model
         $menuItem = $this;
         while ($menuItem->parent_menu_item_id) {
             $menuItem = self::find($menuItem->parent_menu_item_id);
-            if (!$menuItem) {
+            if (! $menuItem) {
                 return;
             }
         }
@@ -168,7 +168,7 @@ class MenuItem extends Model
     public function getUrl()
     {
         return Cache::tags(['menus', 'menu-items', 'products', 'product-categories', 'pages', 'articles', "menuitem-$this->id"])->remember("menuitem-url-$this->id-" . App::getLocale(), 60 * 60 * 24, function () {
-            if (!$this->type || $this->type == 'normal' || $this->type == 'external_url') {
+            if (! $this->type || $this->type == 'normal' || $this->type == 'external_url') {
                 if ($this->url && (parse_url($this->url)['host'] ?? request()->getHttpHost()) != request()->getHttpHost()) {
                     return $this->url;
                 } else {
@@ -190,14 +190,14 @@ class MenuItem extends Model
     public function name()
     {
         return Cache::tags(['menus', 'menu-items', 'products', 'product-categories', 'pages', 'articles', "menuitem-$this->id"])->remember("menuitem-name-$this->id-" . App::getLocale(), 60 * 60 * 24, function () {
-            if (!$this->type || $this->type == 'normal' || $this->type == 'external_url') {
+            if (! $this->type || $this->type == 'normal' || $this->type == 'external_url') {
                 return $this->name;
             } else {
                 $modelResult = $this->model::find($this->model_id);
                 $replacementName = '';
                 if ($modelResult) {
                     $replacementName = $modelResult->name;
-                    if (!$replacementName) {
+                    if (! $replacementName) {
                         $replacementName = $modelResult->title;
                     }
                 }
