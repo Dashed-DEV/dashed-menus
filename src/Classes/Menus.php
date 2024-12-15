aaebf85221d76ced64722ba9258e8b1b2ba7ae67
Robin van Maasakker <robin@qubiqx.com>
2024-12-15 15:58:57 +0100

    Update

--- src/Classes/Menus.php
+++ src/Classes/Menus.php
@@ -26,65 +26,43 @@ class Menus
         $menuItems = Cache::rememberForever("menu-$menuName-" . App::getLocale(), function () use ($menuName) {
             $menu = Menu::where('name', $menuName)->first();
 
-            if ($menu) {
-                $menuItems = [];
-                foreach ($menu->parentMenuItems()->with(['childMenuItems'])->thisSite()->get() as $menuItem) {
-                    $childMenuItems = $menuItem->childMenuItems()->with(['childMenuItems'])->get();
-                    $childs = [];
-                    foreach ($childMenuItems as $childMenuItem) {
-                        $child2MenuItems = $childMenuItem->childMenuItems()->with(['childMenuItems'])->get();
-                        $childs2 = [];
-                        foreach ($child2MenuItems as $child2MenuItem) {
-                            $childs2[] = [
-                                'id' => $child2MenuItem->id,
-                                'name' => $child2MenuItem->name(),
-                                'url' => $child2MenuItem->getUrl(),
-                                'type' => $child2MenuItem->type,
-                                'contentBlocks' => $child2MenuItem->contentBlocks,
-                                'hasChilds' => false,
-                                'childs' => [],
-                            ];
-                        }
-
-                        $childs[] = [
-                            'id' => $childMenuItem->id,
-                            'name' => $childMenuItem->name(),
-                            'url' => $childMenuItem->getUrl(),
-                            'type' => $childMenuItem->type,
-                            'contentBlocks' => $childMenuItem->contentBlocks,
-                            'hasChilds' => count($childs2) ? true : false,
-                            'childs' => $childs2,
-                        ];
-                    }
-
-                    $menuItems[] = [
-                        'id' => $menuItem->id,
-                        'name' => $menuItem->name(),
-                        'url' => $menuItem->getUrl(),
-                        'type' => $menuItem->type,
-                        'contentBlocks' => $menuItem->contentBlocks,
-                        'hasChilds' => count($childs) ? true : false,
-                        'childs' => $childs,
-                    ];
-                }
-
-                return $menuItems;
+            if (!$menu) {
+                return [];
             }
 
-            return [];
+            $topLevelMenuItems = $menu->parentMenuItems()->with(['childMenuItems'])->thisSite()->get();
+            return self::processMenuItems($topLevelMenuItems);
         });
 
+        $menuItems = self::setActiveStatus($menuItems);
+
+        return $menuItems;
+    }
+
+    public static function processMenuItems($menuItems)
+    {
+        $result = [];
+        foreach ($menuItems as $menuItem) {
+            $childMenuItems = $menuItem->childMenuItems()->with(['childMenuItems'])->get();
+            $result[] = [
+                'id' => $menuItem->id,
+                'name' => $menuItem->name(),
+                'url' => $menuItem->getUrl(),
+                'type' => $menuItem->type,
+                'contentBlocks' => $menuItem->contentBlocks,
+                'hasChilds' => $childMenuItems->isNotEmpty(),
+                'childs' => $childMenuItems->isNotEmpty() ? self::processMenuItems($childMenuItems) : [],
+            ];
+        }
+        return $result;
+    }
+
+    public static function setActiveStatus($menuItems): array
+    {
         foreach ($menuItems as &$menuItem) {
             $menuItem['active'] = Helper::urlIsActive($menuItem['url'], true);
             if ($menuItem['hasChilds']) {
-                foreach ($menuItem['childs'] as &$childLevel1Item) {
-                    $childLevel1Item['active'] = Helper::urlIsActive($childLevel1Item['url'], true);
-                    if ($childLevel1Item['hasChilds']) {
-                        foreach ($childLevel1Item['childs'] as &$childLevel2Item) {
-                            $childLevel2Item['active'] = Helper::urlIsActive($childLevel2Item['url'], true);
-                        }
-                    }
-                }
+                self::setActiveStatus($menuItem['childs']);
             }
         }
 
